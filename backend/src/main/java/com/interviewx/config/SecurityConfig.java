a524106fbdf8a446ddc1bfe03f6280869e5871d5
Siddharth Kadhane <siddharthkadhane@gmail.com>
2026-07-18 13:47:13 +0530

    Change the filef

--- backend/src/main/java/com/interviewx/config/SecurityConfig.java
+++ backend/src/main/java/com/interviewx/config/SecurityConfig.java
@@ -33,8 +33,8 @@ public class SecurityConfig {
 
         // Updated with your local port and your actual live production Frontend URL
         configuration.setAllowedOrigins(List.of(
-            "http://localhost:5173", 
-            "https://interview-x-unl2.vercel.app"
+    "http://localhost:5173" 
+            
         ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
